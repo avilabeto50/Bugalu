133ceb01dccb429d39fce127621f04aaf686890b
Carlos <carlos30a97@gmail.com>
2025-01-24 14:30:48 -0600

    Footer front end completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <Router>
+   
       <div className="App">
+        <Router>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+        </Router>
+        <Footer />
       </div>
-    </Router>
+    
+    
   );
 };
 
